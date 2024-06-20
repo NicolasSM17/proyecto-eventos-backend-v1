8dcdad75ce3f58fd90610356c88340b621521fb5
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-06-20 14:34:17 -0500

    Inicio de Proyecto

---
+++ src/main/java/pe/proyecto/eventos/ProyectoEventosBackendV1Application.java
@@ -0,0 +1,13 @@
+package pe.proyecto.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEventosBackendV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEventosBackendV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/proyecto/eventos/ProyectoEventosBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.proyecto.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEventosBackendV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
